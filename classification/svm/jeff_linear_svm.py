c42b8b4a36d4f3b9320d7ed8ea50c4fefbff5c4a
Jeff Lord <jeffscottlord@gmail.com>
2017-10-25 10:59:11 -0400

    Update jeff_linear_svm.py

--- classification/svm/jeff_linear_svm.py
+++ classification/svm/jeff_linear_svm.py
@@ -5,9 +5,9 @@ from matplotlib import pyplot as plt
 import plot_boundary_on_data
 ops.reset_default_graph() # Double check this
 
-# Start the graph
-sess = tf.Session()
 
+# Extract Function from file
+# data_dir includes complete directory including extension
 def extract_data(data_dir):
   data = np.genfromtxt(data_dir, skip_header=1)
   labels = data[:,0]
@@ -15,7 +15,31 @@ def extract_data(data_dir):
   # Returns a matrix of labels, and a matrix of features
   return labels,features
 
+
+# Start the graph
+sess = tf.Session()
+
 # Load the data
 # Data should be in the form : First row = header (will be excluded), leftmost column is label (0 or 1), rest is features
 data_dir = ""
 labels, features = extract_data(data_dir)
+labels = np.array([1 if x == 1 else -1 for x in labels]) # Get array of 1 or -1, instead of 0 and 1.
+# Train size = number of entries(trials), num_features = number of features(number of frequencies, or whatever else used)
+train_size, num_features = features.shape
+
+num_epochs = 1
+
+# Separate data into training / testing
+train_indices = np.random.choice(num_features, round(num_features * 0.8), replace = False)
+test_indices = np.array(list(set(range(num_features)) - set(train_indices)))
+training_features = features[train_indices]
+testing_featuers = features[test_indices]
+training_labels = labels[train_indices]
+testing_labels = labels[test_indices]
+
+# Batch size
+batch_size = 100
+x =
+y = 
+A = 
+b = 

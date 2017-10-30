5c0f962533b3e9dd333f351a475b8154e8c5039e
Jeff Lord <jeffscottlord@gmail.com>
2017-10-29 21:23:56 -0400

    Lots more work

--- classification/svm/temp/tests.py
+++ classification/svm/temp/tests.py
@@ -1,18 +1,20 @@
 import random
 import math
 
-for i in range(10000):
+for i in range(1000):
     x = random.uniform(-100.0, 100.0)
     y = random.uniform(-100, 100)
 
-    # if(x > y):
-    #     print("0," + str(x) + "," + str(y))
-    # elif(y > x):
-    #     print("1," + str(x) + "," + str(y))
-
-
-    if(math.sqrt(x*x + y*y) < 100):
-        print("1," + str(x) + "," + str(y))
-    else:
+    if(x>0 and y>0):
+        print("2,"+str(x) + "," + str(y))
+    elif(x > y):
         print("0," + str(x) + "," + str(y))
+    elif(y > x):
+        print("1," + str(x) + "," + str(y))
+
+
+    # if(math.sqrt(x*x + y*y) < 100):
+    #     print("1," + str(x) + "," + str(y))
+    # else:
+    #     print("0," + str(x) + "," + str(y))
 

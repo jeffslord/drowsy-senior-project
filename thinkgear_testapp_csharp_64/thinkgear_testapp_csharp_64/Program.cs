2b183175b98098b3faa7e5224efe3fbc045905ea
Jeff Lord <jeffscottlord@gmail.com>
2018-04-03 12:58:43 -0400

    added auto-backup, not implemented yet

--- thinkgear_testapp_csharp_64/thinkgear_testapp_csharp_64/Program.cs
+++ thinkgear_testapp_csharp_64/thinkgear_testapp_csharp_64/Program.cs
@@ -7,15 +7,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using libStreamSDK;
 
-namespace thinkgear_testapp_csharp_64
-{
-    class Program
-    {
-        static void Main (string[] args)
-        {
+namespace thinkgear_testapp_csharp_64 {
+    class Program {
+        static void Main(string[] args) {
 
-            while (true)
-            {
+            while (true) {
                 bool toFile = true;
                 String userId = "";
                 int userStatus = -1;
@@ -23,86 +19,79 @@ namespace thinkgear_testapp_csharp_64
                 int sampleRate = 512;
                 String savePath = "data/output.csv";
                 String idPath = "data/ids.csv";
+                String backupPath = "data/"
 
                 bool _idFound = false;
-                while (!_idFound)
-                {
-                    Console.Write ("[INPUT] Enter ID: ");
-                    userId = Console.ReadLine ();
-                    _idFound = ValidateId (idPath, userId);
+                while (!_idFound) {
+                    Console.Write("[INPUT] Enter ID: ");
+                    userId = Console.ReadLine();
+                    _idFound = ValidateId(idPath, userId);
                 }
 
-                Console.Write ("[INPUT] Enter number of trials: ");
-                maxTrials = int.Parse (Console.ReadLine ());
-                while (maxTrials < 1)
-                {
-                    Console.Write ("Invalid number of trials, try again: ");
-                    maxTrials = int.Parse (Console.ReadLine ());
+                Console.Write("[INPUT] Enter number of trials: ");
+                maxTrials = int.Parse(Console.ReadLine());
+                while (maxTrials < 1) {
+                    Console.Write("Invalid number of trials, try again: ");
+                    maxTrials = int.Parse(Console.ReadLine());
                 }
 
-                Console.Write ("[INPUT] Enter status for trials (0=closed, 1=open): ");
-                userStatus = int.Parse (Console.ReadLine ());
-                while (userStatus != 1 && userStatus != 0)
-                {
-                    Console.Write ("Invalid status value, try again (0=closed, 1=open): ");
-                    userStatus = int.Parse (Console.ReadLine ());
+                Console.Write("[INPUT] Enter status for trials (0=closed, 1=open): ");
+                userStatus = int.Parse(Console.ReadLine());
+                while (userStatus != 1 && userStatus != 0) {
+                    Console.Write("Invalid status value, try again (0=closed, 1=open): ");
+                    userStatus = int.Parse(Console.ReadLine());
                 }
 
-                CollectData (userId, maxTrials, userStatus, savePath, sampleRate, toFile);
+                CollectData(userId, maxTrials, userStatus, savePath, sampleRate, toFile);
 
             }
             /* Read 10 ThinkGear Packets from the connection, 1 Packet at a time */
 
         }
 
-        public static void CollectData (String userId, int numTrials, int trialStatus, String savePath, int sampleRate, bool toFile)
-        {
+        public static void CollectData(String userId, int numTrials, int trialStatus, String savePath, int sampleRate, bool toFile) {
             #region INITIALIZE
 
-            int trialOffset = GetTrialOffset (savePath, userId, trialStatus);
+            int trialOffset = GetTrialOffset(savePath, userId, trialStatus);
             DateTime previousTime;
             double seconds = 0.0f;
 
-            Console.WriteLine ("[INFO] Initializing headset...");
-            NativeThinkgear thinkgear = new NativeThinkgear ();
-            Console.WriteLine ("Version: " + NativeThinkgear.TG_GetVersion ());
+            Console.WriteLine("[INFO] Initializing headset...");
+            NativeThinkgear thinkgear = new NativeThinkgear();
+            Console.WriteLine("Version: " + NativeThinkgear.TG_GetVersion());
             /* Get a connection ID handle to ThinkGear */
-            int connectionID = NativeThinkgear.TG_GetNewConnectionId ();
-            Console.WriteLine ("Connection ID: " + connectionID);
-            if (connectionID < 0)
-            {
-                Console.WriteLine ("ERROR: TG_GetNewConnectionId() returned: " + connectionID);
+            int connectionID = NativeThinkgear.TG_GetNewConnectionId();
+            Console.WriteLine("Connection ID: " + connectionID);
+            if (connectionID < 0) {
+                Console.WriteLine("ERROR: TG_GetNewConnectionId() returned: " + connectionID);
                 return;
             }
             int errCode = 0;
             /* Set/open stream (raw bytes) log file for connection */
-            errCode = NativeThinkgear.TG_SetStreamLog (connectionID, "streamLog.txt");
-            Console.WriteLine ("errCode for TG_SetStreamLog : " + errCode);
-            if (errCode < 0)
-            {
-                Console.WriteLine ("[ERROR] TG_SetStreamLog() returned: " + errCode);
+            errCode = NativeThinkgear.TG_SetStreamLog(connectionID, "streamLog.txt");
+            Console.WriteLine("errCode for TG_SetStreamLog : " + errCode);
+            if (errCode < 0) {
+                Console.WriteLine("[ERROR] TG_SetStreamLog() returned: " + errCode);
                 return;
             }
             /* Set/open data (ThinkGear values) log file for connection */
-            errCode = NativeThinkgear.TG_SetDataLog (connectionID, "dataLog.txt");
-            Console.WriteLine ("errCode for TG_SetDataLog : " + errCode);
-            if (errCode < 0)
-            {
-                Console.WriteLine ("[ERROR] TG_SetDataLog() returned: " + errCode);
+            errCode = NativeThinkgear.TG_SetDataLog(connectionID, "dataLog.txt");
+            Console.WriteLine("errCode for TG_SetDataLog : " + errCode);
+            if (errCode < 0) {
+                Console.WriteLine("[ERROR] TG_SetDataLog() returned: " + errCode);
                 return;
             }
             /* Attempt to connect the connection ID handle to serial port "COM5" */
             string comPortName = "COM3";
-            errCode = NativeThinkgear.TG_Connect (connectionID,
+            errCode = NativeThinkgear.TG_Connect(connectionID,
                 comPortName,
                 NativeThinkgear.Baudrate.TG_BAUD_57600,
                 NativeThinkgear.SerialDataFormat.TG_STREAM_PACKETS);
-            if (errCode < 0)
-            {
-                Console.WriteLine ("[ERROR] TG_Connect() returned: " + errCode);
+            if (errCode < 0) {
+                Console.WriteLine("[ERROR] TG_Connect() returned: " + errCode);
                 return;
             }
-            Console.WriteLine ("[INFO] Initializing headset finsihed.");
+            Console.WriteLine("[INFO] Initializing headset finsihed.");
             #endregion
 
             #region PROCESS
@@ -111,42 +100,36 @@ namespace thinkgear_testapp_csharp_64
             int packetsRead = 0;
             int currentPacket = 0;
 
-            Console.WriteLine ("[INFO] Starting data collection in 3 seconds...");
-            Thread.Sleep (3000);
+            Console.WriteLine("[INFO] Starting data collection in 3 seconds...");
+            Thread.Sleep(3000);
 
-            while (currentTrial < numTrials)
-            {
+            while (currentTrial < numTrials) {
                 /* Attempt to read a Packet of data from the connection */
-                errCode = NativeThinkgear.TG_ReadPackets (connectionID, 1);
+                errCode = NativeThinkgear.TG_ReadPackets(connectionID, 1);
                 /* If TG_ReadPackets() was able to read a complete Packet of data... */
-                if (errCode == 1)
-                {
+                if (errCode == 1) {
                     /* The raw data was updated since the last call */
-                    if (NativeThinkgear.TG_GetValueStatus (connectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0)
-                    {
+                    if (NativeThinkgear.TG_GetValueStatus(connectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0) {
                         /* Skip the first 2 seconds to avoid bad data */
-                        Console.WriteLine ("[INFO] Skipping packets...");
-                        if (currentPacket < sampleRate * 2)
-                        {
-                            Console.Write ("\r[INFO] Skipping Packet (" + currentPacket + "/" + sampleRate * 2 + ")");
+                        Console.WriteLine("[INFO] Skipping packets...");
+                        if (currentPacket < sampleRate * 2) {
+                            Console.Write("\r[INFO] Skipping Packet (" + currentPacket + "/" + sampleRate * 2 + ")");
                             packetsRead++;
                             currentPacket++;
                             continue;
                         }
-                        if (currentPacket % sampleRate == 0)
-                        {
+                        if (currentPacket % sampleRate == 0) {
                             currentPacket = 0;
                             currentTrial++;
                             previousTime = DateTime.Now;
                             seconds = (DateTime.Now - previousTime).TotalSeconds;
                         }
-                        float _raw = NativeThinkgear.TG_GetValue (connectionID, NativeThinkgear.DataType.TG_DATA_RAW);
+                        float _raw = NativeThinkgear.TG_GetValue(connectionID, NativeThinkgear.DataType.TG_DATA_RAW);
                         DateTime _time = DateTime.Now;
-                        Trial _currentTrial = new Trial (userId, trialStatus, currentTrial + trialOffset, _raw, currentPacket, _time);
-                        Console.WriteLine ("Trial=" + currentTrial + " Packet=" + currentPacket + " UserID=" + userId + " Status=" + trialStatus + " Total_Trial=" + (currentTrial + trialOffset));
-                        if (toFile)
-                        {
-                            InsertTrialData (_currentTrial, savePath);
+                        Trial _currentTrial = new Trial(userId, trialStatus, currentTrial + trialOffset, _raw, currentPacket, _time);
+                        Console.WriteLine("Trial=" + currentTrial + " Packet=" + currentPacket + " UserID=" + userId + " Status=" + trialStatus + " Total_Trial=" + (currentTrial + trialOffset));
+                        if (toFile) {
+                            InsertTrialData(_currentTrial, savePath);
                         }
                         packetsRead++;
                         currentPacket++;
@@ -159,66 +142,71 @@ namespace thinkgear_testapp_csharp_64
             #endregion
 
             #region DISCONNECT
-            NativeThinkgear.TG_Disconnect (connectionID); // disconnect test
+            NativeThinkgear.TG_Disconnect(connectionID); // disconnect test
             /* Clean up */
-            NativeThinkgear.TG_FreeConnection (connectionID);
+            NativeThinkgear.TG_FreeConnection(connectionID);
             /* End program */
-            Console.ReadLine ();
+            Console.ReadLine();
             #endregion
         }
 
-        public static void InsertTrialData (Trial trial, String filePath)
-        {
-            using (var tw = new StreamWriter (filePath, true))
-            {
-                tw.WriteLine (trial);
+        public static void InsertTrialData(Trial trial, String filePath) {
+            using(var tw = new StreamWriter(filePath, true)) {
+                tw.WriteLine(trial);
             }
         }
 
-        public static bool ValidateId (String filePath, String id)
-        {
-            using (var reader = new StreamReader (filePath))
-            {
-                List<string> ids = new List<string> ();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine ();
-                    var values = line.Split (',');
-                    ids.Add (values[0]);
+        public static bool ValidateId(String filePath, String id) {
+            using(var reader = new StreamReader(filePath)) {
+                List<string> ids = new List<string>();
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    ids.Add(values[0]);
                 }
-                if (ids.Contains (id))
+                if (ids.Contains(id))
                     return true;
                 else
                     return false;
             }
         }
-        public static int GetTrialOffset (String filePath, String id, int status)
-        {
-            using (var reader = new StreamReader (filePath))
-            {
-                List<int> trials = new List<int> ();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine ();
-                    var values = line.Split (',');
-                    if (values[0] == id && int.Parse (values[1]) == status && !trials.Contains (int.Parse (values[2])))
-                        trials.Add (int.Parse (values[2]));
+        public static int GetTrialOffset(String filePath, String id, int status) {
+            using(var reader = new StreamReader(filePath)) {
+                List<int> trials = new List<int>();
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    if (values[0] == id && int.Parse(values[1]) == status && !trials.Contains(int.Parse(values[2])))
+                        trials.Add(int.Parse(values[2]));
                 }
                 //! get max of list
                 int max;
                 if (trials.Count > 0)
-                    max = trials.Max ();
+                    max = trials.Max();
                 else
                     max = 0;
 
                 //! return max as offset
                 return max;
             }
+        }
+        public static void BackupSave(String filePath, String backupPath) {
+            String base = "output_backup";
+            int count = 1;
+            String end = ".csv";
+            bool found = false;
 
-            // public static void RunTrial ()
-            // {
-            //     NativeThinkgear.TG_ReadPackets (connectionID);
-            // }
+            string[] files = Directory.GetFiles(backupPath);
+            while (!found) {
+                String path = base + count.ToString() + end;
+                foreach (string fileName in files) {
+                    if (path == Path.GetFileName(fileName)) {
+                        count++;
+                        break;
+                    }
+                    found = true;
+                }
+            }
         }
     }
 }

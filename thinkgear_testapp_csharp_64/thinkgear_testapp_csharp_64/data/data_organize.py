69d5ef289a27acdb4394e32e956cfeca3ab38cc2
Jeff Lord <jeffscottlord@gmail.com>
2018-04-04 16:07:59 -0400

    Added conversion from organized data to fft data

--- thinkgear_testapp_csharp_64/thinkgear_testapp_csharp_64/data/data_organize.py
+++ thinkgear_testapp_csharp_64/thinkgear_testapp_csharp_64/data/data_organize.py
@@ -2,10 +2,10 @@ import csv
 
 DATA_PATH = "output_raw.csv"
 OUTPUT_PATH = "output_organized.csv"
-HEADER = ["id", "status", "trial", "packet"]
+HEADER = ["id", "status", "trial"]
 for i in range(512):
     HEADER.append("raw" + str(i))
-ALL_ORGANIZED = []
+OUTPUT = []
 
 
 with open(DATA_PATH, 'r') as file:
@@ -15,30 +15,28 @@ with open(DATA_PATH, 'r') as file:
     _id = -1
     _status = -1
     _trial = -1
-    _i = 0
     for row in reader:
         if(row[0] != _id or row[1] != _status or row[2] != _trial):
             if(len(_organized) > 0):
                 print("id=" + str(_id) + " status=" +
                       str(_status) + " trial=" + str(_trial))
                 print(_organized)
-                ALL_ORGANIZED.append(_organized)
+                OUTPUT.append(_organized)
             print("[INFO] New trial...")
             _id = row[0]
             _status = row[1]
             _trial = row[2]
             _organized = []
-            _organized.extend([_id, _status, _trial, _i])
+            _organized.extend([_id, _status, _trial])
         _organized.append(row[4])
-        _i += 1
     print("id=" + _id + " status=" + _status + " trial=" + _trial)
     print(_organized)
-    ALL_ORGANIZED.append(_organized)
+    OUTPUT.append(_organized)
 
 f = open(OUTPUT_PATH, "w+")
 f.close()
 with open(OUTPUT_PATH, "w") as file:
     writer = csv.writer(file, delimiter=",")
     writer.writerow(HEADER)
-    for line in ALL_ORGANIZED:
+    for line in OUTPUT:
         writer.writerow(line)
